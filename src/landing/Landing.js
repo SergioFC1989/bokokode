1796f41659666efd0b2faefd0c7fc970c0462b72
SergioFc1989 <sergiofc@hotmail.es>
2022-08-21 21:12:08 +0200

    refactoring code

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -4,39 +4,22 @@ import Featured from './component/Featured';
 import Information from './component/Information';
 import Layout from '../common/component/Layout';
 
-import useFetch from '../api/useFetch';
+import useProducts from './hooks/useProducts';
+import FilterProduct from '../common/component/FilterProduct';
 
 const Landing = () => {
-  const { data, error, loading } = useFetch(
-    'https://technical-frontend-api.bokokode.com/api/products?page=1',
-    { method: 'POST' }
-  )
+  // const { data, error, loading } = useProducts('1')
+  const data = {};
   const [photoDay, setPhotoDay] = useState()
   const [productAlsoBuy, setProductAlsoBuy] = useState()
 
-  useEffect(() => {
-    data.length > 0 && (() => {
-      console.log(data)
-      //Search photo of the day for prop featured === true
-      const foundImageFeatured = data.map(elem => elem.data.find(elem => elem.featured === true))
-      foundImageFeatured !== undefined && (() => {
-        const selectedImageFeatured = foundImageFeatured.map(elem => elem.image.src)
-        setPhotoDay(selectedImageFeatured)
-      })()
-      //Search products that people also buy
-      const foundProductsAlsoBuy = data.map(elem => elem.data.find(elem => elem.people_also_buy.length > 0))
-      foundProductsAlsoBuy !== undefined && (() => {
-        const selectedProductsAlsoBuy = foundProductsAlsoBuy.flatMap(elem => elem.people_also_buy)
-        setProductAlsoBuy(selectedProductsAlsoBuy)
-      })()
-    })()
-  }, [data])
-
+  const featuredProduct = data.products.find(product => product.featured)
   return (
     <>
       <Layout>
-        <Featured src={photoDay} />
+        <Featured product={featuredProduct} />
         <Information data={productAlsoBuy} />
+        <FilterProduct />
       </Layout>
     </>
   );
